2fae151a0cd9b01c80b2d072837109d793968cd4
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-01-25 16:12:04 +0100

    Created Project

---
+++ SubsHub/SubsHub/SubsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubsHubApp.swift
+//  SubsHub
+//
+//  Created by Domenico Cafarelli on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SubsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
